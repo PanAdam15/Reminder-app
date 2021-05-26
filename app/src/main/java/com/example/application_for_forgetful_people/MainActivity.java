26940b0a8635bcae69ea98d5c43dad4866a57ce3
adamos98 <Zal.pl13579*>
2021-05-26 12:04:13 +0200

    NewReminder commit

--- app/src/main/java/com/example/application_for_forgetful_people/MainActivity.java
+++ app/src/main/java/com/example/application_for_forgetful_people/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.application_for_forgetful_people;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addNewReminderButton = (Button) findViewById(R.id.AddNewReminder);
+        addNewReminderButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewReminder.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/application_for_forgetful_people/NewReminder.java
@@ -0,0 +1,13 @@
+package com.example.application_for_forgetful_people;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_reminder);
+    }
+}
