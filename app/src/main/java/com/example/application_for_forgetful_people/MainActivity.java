dc64d3b2be8580917661d448fa18b21e8b0bc539
adamx <adamxpankowski@gmail.com>
2021-05-27 10:53:15 +0200

    listview commit

--- app/src/main/java/com/example/application_for_forgetful_people/MainActivity.java
+++ app/src/main/java/com/example/application_for_forgetful_people/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.application_for_forgetful_people;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     Button addNewReminderButton;
     Button settingsButton;
+    ListView list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
         addNewReminderButton = (Button) findViewById(R.id.AddNewReminder);
         settingsButton = (Button) findViewById(R.id.settingsButton);
-
+        list = (ListView) findViewById(R.id.listView1);
+        String reminders[] = {"Pranie", "Lalala", "Marmolada"};
+        ArrayList<String> reminderL = new ArrayList<String>();
+        reminderL.addAll(Arrays.asList(reminders));
+        adapter = new ArrayAdapter<String>(this,R.layout.activity_listview,reminderL);
+        list.setAdapter(adapter);
         addNewReminderButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
