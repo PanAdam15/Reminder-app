cea8fe1f7f4e1fe8a561881096afec8932ac9497
adamos98 <adamosiak1998@gmail.com>
2021-07-28 17:51:01 +0200

    Run notification depends on day in system

--- app/src/main/java/com/example/application_for_forgetful_people/dao/ReminderDao.java
+++ app/src/main/java/com/example/application_for_forgetful_people/dao/ReminderDao.java
@@ -18,7 +18,13 @@ public interface ReminderDao {
     @Query("SELECT * FROM reminder")
     LiveData<List<Reminder>> getAllReminders();
 
-    @Query("SELECT * FROM reminder where isActive = 1")
+    @Query("SELECT * FROM reminder where isActive = 1 and ((SELECT strftime('%w','now') = '0' and ifSunday = 1) or " +
+            "(SELECT strftime('%w','now') = '1' and ifMonday = 1) or " +
+            "(SELECT strftime('%w','now') = '2' and ifTuesday = 1) or " +
+            "(SELECT strftime('%w','now') = '3' and ifWednesday = 1) or " +
+            "(SELECT strftime('%w','now') = '4' and ifThursday = 1) or " +
+            "(SELECT strftime('%w','now') = '5' and ifFriday = 1) or " +
+            "(SELECT strftime('%w','now') = '6' and ifSaturday = 1))")
     LiveData<List<Reminder>> getRemindersWhoseAreActive();
 
     @Delete
